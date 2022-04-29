3728fefceda83aa2631ea28c45c5f188f398fb8a
PrakashRK17 <videomemes17@gmail.com>
2022-04-29 12:12:35 +0530

    new

--- src/org/sample/MyPro.java
+++ src/org/sample/MyPro.java
@@ -2,11 +2,15 @@ package org.sample;
 
 public class MyPro {
 	private void tc1() {
-		System.out.println("123");
+		System.out.println("321");
+	}
+	private void tc2() {
+		System.out.println("mani");
 	}
 	public static void main(String[] args) {
 		MyPro m = new MyPro();
 		m.tc1();
+		m.tc2();
 	}
 
 }
