a9d542256541458acfadaff9435ac29f265d31fa
PrakashRK17 <104447571+PrakashRK17@users.noreply.github.com>
2022-04-29 12:21:19 +0530

    Update MyPro.java

--- src/org/sample/MyPro.java
+++ src/org/sample/MyPro.java
@@ -7,11 +7,15 @@ public class MyPro {
 	private void tc2() {
 		System.out.println("mani");
 	}
+	private void tc3() {
+		System.out.println("jp");
+	}
 	
 	public static void main(String[] args) {
 		MyPro m = new MyPro();
 		m.tc1();
 		m.tc2();
+		m.tc3();
 	}
 
 }
