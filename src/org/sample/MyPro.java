45829bc47a459738e4d7aed074bff899301b488f
PrakashRK17 <104447571+PrakashRK17@users.noreply.github.com>
2022-04-29 11:48:52 +0530

    Update MyPro.java

--- src/org/sample/MyPro.java
+++ src/org/sample/MyPro.java
@@ -2,8 +2,9 @@ package org.sample;
 
 public class MyPro {
 	private void tc1() {
-		System.out.println("123");
+		System.out.println("321");
 	}
+	
 	public static void main(String[] args) {
 		MyPro m = new MyPro();
 		m.tc1();
